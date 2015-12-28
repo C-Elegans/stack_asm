f5c6303525eef4d4497fccf7b7bd25910c8e43c5
C-Elegans <mtnolan2640@gmail.com>
2015-12-28 10:50:39 -0500

    made __init__.py more module friendly

--- stack_as/__init__.py
+++ stack_as/__init__.py
@@ -1,40 +1,44 @@
 #!/usr/bin/python
 import isa
 import os,sys,re
-f = open(sys.argv[1],"r")
-lines = f.readlines()
-out = open(sys.argv[2],"wb")
-label_pattern = re.compile(r"^.*:") #matches a label
-arr = []
+def main():
+    f = open(sys.argv[1],"r")
+    lines = f.readlines()
+    out = open(sys.argv[2],"wb")
+    label_pattern = re.compile(r"^.*:") #matches a label
+    arr = []
 
-byte_counter = 0
-for line in lines:
-    line = re.sub(r"\;.*","",line)
-    line = line.strip()
-    if line:
-        tokens = re.split(r"[ ,]",line)
+    byte_counter = 0
+    for line in lines:
+        line = re.sub(r"\;.*","",line)
+        line = line.strip()
+        if line:
+            tokens = re.split(r"[ ,]",line)
         
-        if label_pattern.match(tokens[0]):
-            label = tokens[0][:-1]
-            print "label: " + label + " at: " + str(byte_counter)
-            isa.labels[label] = byte_counter
-            print isa.labels
-            tokens.pop(0)
-            if len(tokens) == 0:
-                continue
-        byte_counter += isa.getInstructionBytes(tokens)
+            if label_pattern.match(tokens[0]):
+                label = tokens[0][:-1]
+                print "label: " + label + " at: " + str(byte_counter)
+                isa.labels[label] = byte_counter
+                print isa.labels
+                tokens.pop(0)
+                if len(tokens) == 0:
+                    continue
+            byte_counter += isa.getInstructionBytes(tokens)
   
-for line in lines:
-    line = re.sub(r"\;.*","",line)
-    line = line.strip()
-    if line:
-        tokens = re.split(r"[ ,]",line)
-        if label_pattern.match(tokens[0]):
-            tokens.pop(0)
-            if len(tokens) == 0:
-                continue
-        ret = isa.decodeInstruction(tokens)
-        arr.extend(ret)
-print arr
-byte_array = bytearray(arr)
-out.write(byte_array)
+    for line in lines:
+        line = re.sub(r"\;.*","",line)
+        line = line.strip()
+        if line:
+            tokens = re.split(r"[ ,]",line)
+            if label_pattern.match(tokens[0]):
+                tokens.pop(0)
+                if len(tokens) == 0:
+                    continue
+            ret = isa.decodeInstruction(tokens)
+            arr.extend(ret)
+    print arr
+    byte_array = bytearray(arr)
+    out.write(byte_array)
+
+if __name__ == "__main__":
+    main()
